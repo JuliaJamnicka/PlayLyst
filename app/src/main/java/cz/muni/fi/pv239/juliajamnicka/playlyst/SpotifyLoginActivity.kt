c9c7bc85a9ac64cf12ae5bb7c6e2e77a7547d9c3
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-03-17 19:29:51 +0100

    minor changes in the authorization flow

--- app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/SpotifyLoginActivity.kt
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/SpotifyLoginActivity.kt
@@ -1,12 +1,33 @@
 package cz.muni.fi.pv239.juliajamnicka.playlyst
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import cz.muni.fi.pv239.juliajamnicka.playlyst.R
+import android.util.Log
+import cz.muni.fi.pv239.juliajamnicka.playlyst.databinding.ActivityMainBinding
+import cz.muni.fi.pv239.juliajamnicka.playlyst.ui.authentication.SpotifyAuthorizationFragment
 
 class SpotifyLoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spotify_login)
+
+        supportFragmentManager
+            .setFragmentResultListener("request_code", this) { _, bundle ->
+                val code = bundle.getString("response_code")
+
+                val preferences = getPreferences(Context.MODE_PRIVATE)
+                with (preferences.edit()) {
+                    putString("spotify_authorization_code", code)
+                    apply()
+                }
+
+                Log.d("SPOT_AUTH_CODE", preferences.getString("spotify_authorization_code", "")!!)
+            }
     }
 }
--- app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/ui/authentication/SpotifyAuthorizationFragment.kt
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/ui/authentication/SpotifyAuthorizationFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.os.bundleOf
 import com.spotify.sdk.android.auth.AuthorizationClient
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
@@ -19,13 +20,15 @@ class SpotifyAuthorizationFragment : Fragment() {
 
     private lateinit var binding: FragmentSpotifyAuthorizationBinding
 
+    //just temporary to not make Spotify sus of the repeated calls
+    private var code: String = "AQDPjxbTMll7fkCi-ei80WOF93OXZdEvPO7kzrBdRZ6oFrvc3FLsjaaI4FURssZc4mNYU27GZu7ZxmDPb1MY4qdoMoBeHNGxylP3Yfp0CLiMIBn175kNx9_URpiew4A0ClKK04AwKmFPJUcL7NTPe-HVjYFTq5i7KrAmuPkhEmtg8g_i5CDlrTNiL41T6BF0-n-k25QD9gb_1m6ObJqK3-p76avW2zMiy5iT6EK_sFZcVurG2istT3cuXBv2-0W25jv6VnIyBzAYVJ03UmywfqI9WB8_IOdTu4XZbpdNKyY5ULgAVv5ghEeLN25qTp5jo37zao4PdaE"
+
     private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == Activity.RESULT_OK) {
             val response = AuthorizationClient.getResponse(it.resultCode, it.data)
 
             if (response.type === AuthorizationResponse.Type.CODE) {
-                val code = response.code
-                //return this to higher activity
+                code = response.code
             }
         }
     }
@@ -39,10 +42,14 @@ class SpotifyAuthorizationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.loginButton.setOnClickListener {
-            val intent = createSpotifyAuthenticationIntent()
-            launcher.launch(intent)
+            if (code.isEmpty()) {
+                val intent = createSpotifyAuthenticationIntent()
+                launcher.launch(intent)
+            }
         }
 
+        parentFragmentManager.setFragmentResult("request_code",
+            bundleOf("response_code" to code))
     }
 
     private fun createSpotifyAuthenticationIntent(): Intent {
