3ea382fe860689cd812722a5b5ffcc5e5ccf659a
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-03-28 20:57:45 +0200

    added spotify-app-remote lib

--- app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/api/SpotifyWebApiService.kt
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/api/SpotifyWebApiService.kt
@@ -1,4 +1,10 @@
 package cz.muni.fi.pv239.juliajamnicka.playlyst.api
 
+import retrofit2.http.GET
+
 interface SpotifyWebApiService {
+    @GET("recommendations")
+    fun getRecommendations(
+
+    )
 }
---
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/api/response/RecommendationsResponse.kt
@@ -0,0 +1,5 @@
+package cz.muni.fi.pv239.juliajamnicka.playlyst.api.response
+
+data class RecommendationsResponse(
+    val Tracks : List<String>
+)
