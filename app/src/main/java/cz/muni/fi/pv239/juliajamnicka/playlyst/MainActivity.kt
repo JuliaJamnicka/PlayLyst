5fc0318c98e2791a7b29ae7a21b818452df6bad2
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-03-28 17:56:49 +0200

    added working navigation, deleted unnecessary login activity

--- app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/MainActivity.kt
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/MainActivity.kt
@@ -2,12 +2,19 @@ package cz.muni.fi.pv239.juliajamnicka.playlyst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import cz.muni.fi.pv239.juliajamnicka.playlyst.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
-
-
 }
