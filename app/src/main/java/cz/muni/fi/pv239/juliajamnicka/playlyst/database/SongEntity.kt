e121f20a3063f493aa01a67c5228df4225a8eb98
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-04-12 23:58:27 +0200

    added entity annotation to songEntity

--- app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/database/SongEntity.kt
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/database/SongEntity.kt
@@ -1,7 +1,9 @@
 package cz.muni.fi.pv239.juliajamnicka.playlyst.database
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class SongEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
