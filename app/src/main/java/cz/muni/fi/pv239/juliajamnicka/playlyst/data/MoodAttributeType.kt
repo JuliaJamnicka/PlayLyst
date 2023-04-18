24a542dca8f2742eb8842dc72669bec9a1f49b04
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-04-18 17:11:59 +0200

    added enum class for possible mood attributes

---
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/data/MoodAttributeType.kt
@@ -0,0 +1,6 @@
+package cz.muni.fi.pv239.juliajamnicka.playlyst.data
+
+enum class MoodAttributeType {
+    ACOUSTICNESS, DANCEABILITY, ENERGY, INSTRUMENTALNESS, KEY, LIVENESS,
+    LOUDNESS, MODE, POPULARITY, SPEECHINESS, TEMPO, VALENCE
+}
