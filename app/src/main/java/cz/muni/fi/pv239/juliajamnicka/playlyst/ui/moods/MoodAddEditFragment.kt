6e554c8a83d3db5d587b7f6565e23fc89d8771b9
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-04-18 16:39:15 +0200

    fixes to moodAddEditFragment

--- app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/ui/moods/MoodAddEditFragment.kt
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/ui/moods/MoodAddEditFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import cz.muni.fi.pv239.juliajamnicka.playlyst.data.Mood
 import cz.muni.fi.pv239.juliajamnicka.playlyst.databinding.FragmentMoodAddEditBinding
 import cz.muni.fi.pv239.juliajamnicka.playlyst.repository.MoodRepository
 
 
 class MoodAddEditFragment : Fragment() {
     private lateinit var binding: FragmentMoodAddEditBinding
+
     private val moodRepository: MoodRepository by lazy {
         MoodRepository(requireContext())
     }
+
+    private val adapter : MoodAttributesAdapter by lazy {
+        MoodAttributesAdapter(
+            onItemClick = { moodAttribute -> {}
+            }
+        )
+    }
+    private val args: MoodAddEditFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +33,26 @@ class MoodAddEditFragment : Fragment() {
     ): View {
         binding = FragmentMoodAddEditBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = adapter
+
+        setInitialValues()
+
+    }
+
+    fun refreshList() {
+        adapter.submitList(args.mood?.attributes)
+    }
+
+    private fun setInitialValues() {
+        if (args.mood != null) {
+
+        }
     }
 
 
