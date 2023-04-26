40894ed72f5e51438f5176f4ac978c6b492e885c
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-04-26 11:18:16 +0200

    deleted unnecessary mood attributes

--- app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/data/MoodAttributeType.kt
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/data/MoodAttributeType.kt
@@ -10,8 +10,8 @@ data class AttributeThresholds(
 )
 
 enum class MoodAttributeType {
-    DANCEABILITY, ACOUSTICNESS, POPULARITY, MODE, INSTRUMENTALNESS, ENERGY, LIVENESS,
-    LOUDNESS, SPEECHINESS, TEMPO, VALENCE, KEY;
+    DANCEABILITY, ACOUSTICNESS, POPULARITY, VALENCE, MODE, INSTRUMENTALNESS, ENERGY,
+    TEMPO, KEY, LIVENESS;
 
     fun getDescription(): String = when (this) {
         KEY -> "The estimated overall key of the track. Integers map to pitches using standard Pitch Class notation. Ex: 0 = C, 1 = C♯/D♭, 2 = D, and so on. If no key was detected, the value is -1."
@@ -20,12 +20,10 @@ enum class MoodAttributeType {
         DANCEABILITY -> "Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is the most danceable."
         ENERGY -> "Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy."
         INSTRUMENTALNESS -> "Predicts whether a track contains no vocals. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content."
-        LOUDNESS -> "The overall loudness of a track in decibels (dB). Values typical range between -60 and 0 dB."
         VALENCE -> "A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive."
         TEMPO -> "The overall estimated tempo of a track in beats per minute (BPM)."
         POPULARITY -> "The popularity of the track. The value will be between 0 and 100, with 100 being the most popular."
         LIVENESS -> "Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live."
-        SPEECHINESS -> "Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks."
     }
 
     fun getThresholds(): AttributeThresholds = when (this) {
@@ -79,15 +77,6 @@ enum class MoodAttributeType {
             lowerDefaultValue = 0.0,
             upperDefaultValue = 1.0
         )
-        LOUDNESS -> AttributeThresholds(
-            minValue = -60.0,
-            maxValue = 0.0,
-            stepSize = 5.0,
-            canHaveRange = true,
-            defaultValue = 0.0,
-            lowerDefaultValue = -60.0,
-            upperDefaultValue = 0.0
-        )
         VALENCE -> AttributeThresholds(
             minValue = 0.0,
             maxValue = 1.0,
@@ -116,15 +105,6 @@ enum class MoodAttributeType {
             upperDefaultValue = 100.0
         )
         LIVENESS -> AttributeThresholds(
-            minValue = 0.0,
-            maxValue = 1.0,
-            stepSize = 0.05,
-            canHaveRange = true,
-            defaultValue = 0.5,
-            lowerDefaultValue = 0.0,
-            upperDefaultValue = 1.0
-        )
-        SPEECHINESS -> AttributeThresholds(
             minValue = 0.0,
             maxValue = 1.0,
             stepSize = 0.05,
