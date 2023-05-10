dfe472f6c401db4c9cc980a32fc923facc835beb
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-05-10 13:53:07 +0200

    fixed token authenticator cycling on token refresh

--- app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/api/TokenAuthenticator.kt
+++ app/src/main/java/cz/muni/fi/pv239/juliajamnicka/playlyst/api/TokenAuthenticator.kt
@@ -8,31 +8,30 @@ import okhttp3.*
 class TokenAuthenticator: Authenticator {
     override fun authenticate(route: Route?, response: Response): Request? {
         return runBlocking {
-            when (val token = getUpdatedToken()) {
-                token -> {
-                    response.request.newBuilder()
-                        .header("Authorization", "Bearer $token")
-                        .build()
-                }
-                else -> null
+            var newToken: String? = null
+
+            val tt = SessionManager.getToken("access_token")
+
+            if (SessionManager.getToken("refresh_token") == null) {
+                SessionManager.getAccessToken(
+                    success = { accessToken -> newToken = accessToken },
+                    fail = { Log.e(this::class::simpleName.toString(), "did not work") })
+            } else {
+                SessionManager.refreshToken(
+                    success = { accessToken -> newToken = accessToken },
+                    fail = { Log.e(this::class::simpleName.toString(), "did not work") })
             }
+
+            val pp = SessionManager.getToken("access_token")
+            (pp != tt)
+
+            if (newToken != null) {
+                SessionManager.saveToken("access_token", newToken!!)
+                response.request.newBuilder()
+                    .header("Authorization", "Bearer $newToken")
+                    .build()
+            }
+            null
         }
     }
-
-    private fun getUpdatedToken(): String? {
-        var newToken: String? = null
-
-        if (SessionManager.getToken("refresh_token") == null) {
-            SessionManager.getAccessToken(
-                success = { accessToken -> newToken = accessToken },
-                fail = { Log.e(this::class::simpleName.toString(), "did not work") })
-        } else {
-            SessionManager.refreshToken(
-                success = { accessToken -> newToken = accessToken },
-                fail = { Log.e(this::class::simpleName.toString(), "did not work") })
-        }
-
-        return newToken
-    }
-
 }
